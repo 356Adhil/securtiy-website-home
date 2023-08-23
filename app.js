13c5a7fb796c1b4ccae790a3b3172a27edf73f12
Adhil Akbar <356adhil@gmail.com>
2023-08-23 10:35:24 +0530

    server running back to www file.

--- app.js
+++ app.js
@@ -98,10 +98,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 module.exports = app;
